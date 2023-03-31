d1aa2d2347c9348745df21204c38ec071dd2eba4
Remmytech <43222501+Remmytech@users.noreply.github.com>
2023-03-31 11:38:03 +0100

    Delete singing1.js

--- singing1.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0]
-const nav = document.getElementsByClassName('nav-bar-links')[0]
-
-toggleButton.addEventListener('click', ()=>{
-    nav.classList.toggle('active')
-})
